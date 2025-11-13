369bec169cb1db19025ee42f2edb62928f0e3eab
DevJuniorss <devjuniorss@gmail.com>
2025-11-13 19:20:13 -0300

    Inicializando repositorio

---
+++ build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    id("com.android.application") version "8.2.0" apply false
+    id("com.android.library") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+
+    // ESTE AQUI É O QUE FALTA!
+    id("com.google.gms.google-services") version "4.4.1" apply false
+}
