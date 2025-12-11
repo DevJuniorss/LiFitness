7bd9dc7c5c866b17e06a9f6b41cb38c4154c17e7
DevJuniorss <114004506+DevJuniorss@users.noreply.github.com>
2025-12-10 20:09:06 -0500

    Fix: Cleanup unused imports and redundant dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,19 +69,14 @@ dependencies {
 
     // Navigation component
     implementation("androidx.navigation:navigation-compose:2.5.3")
-    implementation("com.google.firebase:firebase-auth:22.3.0")
 
-    // Testing libraries
+    // Firebase dependencies
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
     implementation("com.google.firebase:firebase-database:20.3.0")
     implementation("com.google.firebase:firebase-firestore:24.9.1")
     implementation("com.google.firebase:firebase-storage:20.3.0")
-    //implementation("com.google.firebase:firebase-auth:22.3.0")
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    implementation("com.google.firebase:firebase-auth-ktx")
 
     // YCharts library
     implementation("co.yml:ycharts:2.1.0")
@@ -90,30 +85,28 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
-    // Firebase dependencies
-    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
-    implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("com.google.firebase:firebase-firestore:24.9.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.0-alpha01")
-    //implementation("com.google.firebase:firebase-auth-ktx")
-
     // Compose Material Design components
     implementation("androidx.compose.material:material-icons-extended:1.5.3")
-    implementation("androidx.compose.material:material:1.1.0-alpha06")
 
     // Navigation component versions
     val nav_version = "2.5.3"
     implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
     implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
 
+    // Lifecycle dependencies
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
     // Others
-    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
-    implementation("androidx.compose.material:material:1.1.0-alpha06")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     implementation("io.coil-kt:coil-compose:1.3.2")
     implementation("com.pierfrancescosoffritti.androidyoutubeplayer:chromecast-sender:0.28")
+
+    // Testing libraries
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 }
